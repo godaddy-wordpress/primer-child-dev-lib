980476b19e21b2b24991c812918f3c6b3fc88541
Frankie Jarrett <fjarrett@gmail.com>
2017-02-16 10:33:44 -0600

    Grab entire tests dir

--- install-wp-tests.sh
+++ install-wp-tests.sh
@@ -79,9 +79,7 @@ install_test_suite() {
 	if [ ! -d $WP_TESTS_DIR ]; then
 		# set up testing suite
 		mkdir -p $WP_TESTS_DIR
-		download https://develop.svn.wordpress.org/${WP_TESTS_TAG}/tests/phpunit/build.xml $WP_TESTS_DIR/build.xml
-		svn co --quiet https://develop.svn.wordpress.org/${WP_TESTS_TAG}/tests/phpunit/includes/ $WP_TESTS_DIR/includes
-		svn co --quiet https://develop.svn.wordpress.org/${WP_TESTS_TAG}/tests/phpunit/data/ $WP_TESTS_DIR/data
+		svn co --quiet https://develop.svn.wordpress.org/${WP_TESTS_TAG}/tests/phpunit/ $WP_TESTS_DIR/
 	fi
 
 	if [ ! -f wp-tests-config.php ]; then
